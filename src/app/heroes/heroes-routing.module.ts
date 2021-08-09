3fe24ae9d3cb49f79fc505e1ea116a2ceee34a12
Daniel Leon <danielleond99@gmail.com>
2021-08-09 14:10:19 -0400

    Routes Added

---
+++ src/app/heroes/heroes-routing.module.ts
@@ -0,0 +1,50 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { ListComponent } from './pages/list/list.component';
+import { AddComponent } from './pages/add/add.component';
+import { SearchComponent } from './pages/search/search.component';
+import { HeroeComponent } from './pages/heroe/heroe.component';
+
+const heroesRoutes: Routes = [
+
+  {
+    path: '',
+    children: [
+      {
+        path: 'list',
+        component: ListComponent
+      },
+      {
+        path: 'add',
+        component: AddComponent
+      },
+      {
+        path: 'edit/:id',
+        component: AddComponent
+      },
+      {
+        path: 'search',
+        component: SearchComponent
+      },
+      {
+        path: ':id',
+        component: HeroeComponent
+      },
+      {
+        path: '**',
+        redirectTo: 'list'
+      }
+    ]
+  }
+]
+
+@NgModule({
+  imports: [
+    RouterModule.forChild(heroesRoutes)
+  ],
+  exports: [
+    RouterModule
+  ]
+})
+export class HeroesRoutingModule { }
