3955ff7a397254c4d2db76b1e909cb242559e8a6
Daniel Leon <danielleond99@gmail.com>
2021-08-10 11:03:24 -0400

    Heroes Service

--- src/app/heroes/pages/list/list.component.ts
+++ src/app/heroes/pages/list/list.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { HeroesService } from '../../services/heroes.service';
+import { IHeroe } from '../../interfaces/heroe.interface';
 
 @Component({
   selector: 'app-list',
@@ -8,9 +10,14 @@ import { Component, OnInit } from '@angular/core';
 })
 export class ListComponent implements OnInit {
 
-  constructor() { }
+  heroes: IHeroe[] = [];
+
+  constructor(
+    private readonly heroesService: HeroesService
+  ) { }
 
   ngOnInit(): void {
-  }
 
+    this.heroesService.getHeroes().subscribe(heroes => this.heroes = heroes);
+  }
 }
---
+++ src/app/heroes/services/heroes.service.ts
@@ -0,0 +1,20 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+
+import { Observable } from 'rxjs';
+
+import { IHeroe } from '../interfaces/heroe.interface';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class HeroesService {
+
+  constructor(
+    private readonly httpClient: HttpClient
+  ) { }
+
+  getHeroes(): Observable<IHeroe[]> {
+    return this.httpClient.get<IHeroe[]>('http://localhost:3000/heroes');
+  }
+}
